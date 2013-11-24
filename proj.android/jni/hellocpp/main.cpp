d8861c967bf80a019d7e41a4f0449f5617783fa9
blwang <wangbole226@gmail.com>
2013-11-24 16:56:36 +0800

    Fixed cocos2d-x 3.0 alpha
    EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,

--- proj.android/jni/hellocpp/main.cpp
+++ proj.android/jni/hellocpp/main.cpp
@@ -13,7 +13,4 @@ using namespace cocos2d;
 void cocos_android_app_init (struct android_app* app) {
     LOGD("cocos_android_app_init");
     AppDelegate *pAppDelegate = new AppDelegate();
-    EGLView* eglView = EGLView::getInstance();
-    eglView->setFrameSize(480, 800);
-    eglView->setDesignResolutionSize(480, 800, ResolutionPolicy::NO_BORDER);
 }
